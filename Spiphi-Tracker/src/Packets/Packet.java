0a9896a6f7b1ae4b2faca08051d44a975ba52fdd
Bennett DenBleyker <bdbleyker@gmail.com>
2017-10-04 19:22:59 -0600

    Packet.toString()

--- Spiphi-Tracker/src/Packets/Packet.java
+++ Spiphi-Tracker/src/Packets/Packet.java
@@ -7,6 +7,7 @@ package Packets;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  *
@@ -20,6 +21,26 @@ public class Packet {
     public Packet(int type, char[] data) {
         this.type = type;
         this.data = data;
+    }
+    
+    @Override
+    public String toString() {
+        String out = "Packet{";
+        switch (type) {
+            case 0:
+                out += "Empty: "; 
+                break;
+            case 1:
+                out += "Ping: ";
+                break;
+            case 2:
+                out += "PingResponse: ";
+                break;
+            default:
+                out += "Other: ";
+        }
+        
+        return out + Arrays.toString(data) + "}";
     }
 
     public static Packet parse(int type, char[] data) {
